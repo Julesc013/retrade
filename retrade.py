de43d397f7e14371b00c2456066acacace30744f
Jules Carboni <30209022+Julesc013@users.noreply.github.com>
2020-07-14 22:47:18 +1000

    did the maths code

--- retrade.py
+++ retrade.py
@@ -10,9 +10,33 @@ print("Retrade v" + VERSION + ", (C) Jules Carboni 2020.") # Print copyright and
 # Import dependencies
 from datetime import datetime
 from datetime import timedelta
+from pytz import timezone
 from time import sleep
 
 from yahoo_fin import stock_info # Import stock_info module from yahoo_fin
+
+
+
+def print_info(ticker, live_price, live_time, iteration, current_stop, submitted_stop):
+    # Format and print live price and other information to console.
+
+    time_stamp = "[" + live_time.strftime(DATE_TIME_FORMAT) + "]" # Time stamp displaying date and time of price retrieval
+    ticker_stamp = ticker.upper() + ": $" # e.g. TSLA: $1750.3487905234
+    
+    stop_prices = "(CS: " + str(round(current_stop, 2)) + ", SS: " + str(round(submitted_stop, 2)) + ")" # The prices of the stops, rounded to the nearest cent
+    #iteration_number = "[" str(iteration) "]" # TEMP
+    
+    print(time_stamp + " " + ticker_stamp + str(live_price) + " " + stop_prices) # Display live price
+
+
+
+# Built in variables
+
+TIMEZONE = timezone('US/Eastern')
+DATE_TIME_FORMAT = "%Y-%m-%d %H:%M:%S %Z%z"
+
+MARKET_OPEN_TIME = 00000 # TEMP
+MARKET_CLOSE_TIME = 00000 # TEMP
 
 
 
@@ -24,7 +48,7 @@ trade_type = input("Trade type (leave blank for 'trailing sell'): ") # Type of t
 volume = int(input("Stocks to trade (INTEGERS ONLY): ")) # Number of stocks to buy/sell
 
 # Calculate the finishing time for the service
-finish_time = datetime.now() + timedelta(seconds=duration)
+finish_time = datetime.now(TIMEZONE) + timedelta(seconds=duration)
 
 
 # Branch out into different trade methods
@@ -33,22 +57,51 @@ if trade_type == "trailing sell" or trade_type == "": # If trade type not specif
 
     # Get trade type specific information
     trail_size = float(input("Trail value (dollars below highest market value to sell at): ")) # Get the size/value of the trail
+    update_zone_prop = float(input("Update zone size (percentage) (lower percentage, higher risk): ")) / 100 # Area between the max price and stop price. If live price falls within this lower region, the remote stop price is updated
+    #current_stop = float(input("Current stop price: "))
+    trade_url = input("URL of trade page (MUST ALREADY HAVE STOP LOSS SET UP): ") # The URL to the stop-loss that has already been set up.
+
+
+    # Get current/submitted stop price
+    submitted_stop = 000000 # TEMP
+    current_stop = submitted_stop
+
+    # Calculate price at which to update the stop
+    update_zone = update_zone_prop * trail_size
+    update_stop = current_stop + update_zone
+
 
     iteration = 0 # Counter for number of passes
 
     # Loop until duration reached (or forever if no duration specified)
-    while datetime.now() < finish_time or duration == 0:
+    while datetime.now(TIMEZONE) < finish_time or duration == 0:
 
 
         iteration += 1
         
-
         # Get live price of stock
         live_price = stock_info.get_live_price(ticker)
+        live_time = datetime.now(TIMEZONE) # The time at which the price was retrieved (or close enough to) # TEMP (use proper datatable function that contains real time)
 
 
-        header = "[" + str(iteration) + "] " + ticker.upper() + ": $" # Format header for when price gets displayed
-        print(header + str(live_price)) # Display live price
+        calculated_stop = live_price - trail_size # The stop price assuming current price is highest price
+
+        # Check if the new stop price is higher than the old stop price
+        if calculated_stop > current_stop:
+
+            # If so, update the current stop price and the stop price at which to update the submitted stop
+            current_stop = calculated_stop
+            update_stop = current_stop + update_zone
+
+        # If the current stop is higher than the submitted stop, and the live price has fallen into the update zone, update the submitted stop
+        if current_stop > submitted_stop and live_price <= update_stop:
+
+            # TEMP Update the trade!
+
+            submitted_stop = current_stop
+
+        
+        print(str(live_price)) # Display live price and information TEMP
 
 
         sleep(interval) # Wait the specififed time before getting the price again
